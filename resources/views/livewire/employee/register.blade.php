56ae839cce92904b9b3d19dabf585425bab68fae
cleitonbrandao <cbs3dmax@gmail.com>
2024-02-01 18:01:50 -0300

    correction in register employee

--- resources/views/livewire/employee/register.blade.php
+++ resources/views/livewire/employee/register.blade.php
@@ -42,7 +42,7 @@
             @endif
 
             <div class="flex items-center justify-center w-full p-2">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" data-modal-target="small-modal" data-modal-toggle="small-modal" href="#">
                     {{ __('Termos de Cadastro') }}
                 </a>
 
