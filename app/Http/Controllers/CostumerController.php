0e846ca395e66e8d247f4fca1758cf1ae3560f44
cleitonbrandao <cbs3dmax@gmail.com>
2024-02-19 11:41:10 -0300

    costumer controller

--- app/Http/Controllers/CostumerController.php
+++ app/Http/Controllers/CostumerController.php
@@ -7,12 +7,6 @@ use App\Http\Requests\StoreCostumerRequest;
 
 class CostumerController extends Controller
 {
-    protected int $costumer_people_id;
-    protected string $birthday;
-    protected string $email;
-    protected int $cpf;
-
-
     public function store(StoreCostumerRequest $request)
     {
         $validated = $request->validated();
@@ -20,11 +14,6 @@ class CostumerController extends Controller
         {
             return $validated->erros();
         }
-        Costumer::created([
-            'cotumer_people_id' => $this->costumer_people_id,
-            'birthday' => $this->birthday,
-            'email' => $this->email,
-            'cpf' => $this->cpf,
-        ]);
+        Costumer::created($request);
     }
 }
